6a9875ddff374ed900c4a51e254cbd30e472f55e
zhuyue <yue.zhu@nascent.cn>
2024-07-03 15:02:53 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { RouterProvider } from 'react-router-dom';
+import router from './router'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <RouterProvider router={router}/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/pages/user.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div style={{color: '#fff', fontSize: '30px'}}>
+        user
+        </div>
+    </>
+  )
+}
+
+export default App
